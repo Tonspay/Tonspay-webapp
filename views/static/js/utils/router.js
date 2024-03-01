9cfea7b635aba7f0fcf1d9821e1b0d2f355d2aa0
root <root@s25453.vps.hosting>
2024-03-01 09:13:21 +0000

    add router

---
+++ views/static/js/utils/router.js
@@ -0,0 +1,11 @@
+function router_to_index() {
+    location.href = "./index.html"
+}
+
+function router_to_invoice() {
+    location.href = "./page-invoices.html"
+}
+
+function router_to_wallet() {
+    location.href = "./page-wallet.html"
+}
