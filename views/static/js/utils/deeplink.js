b702f8ea01bc8df566606c16a952b6922ff085e9
root <root@s25453.vps.hosting>
2024-03-11 11:39:45 +0000

    adding metamask pay incoive process

--- views/static/js/utils/deeplink.js
+++ views/static/js/utils/deeplink.js
@@ -1,3 +1,7 @@
+let account;
+
+let invoice;
+
 async function phantom_connect_wallet() {
     //Generate a new sign kp
     const pk = await api_preconnect_phantom();
@@ -35,4 +39,44 @@ async function metamask_connect_wallet_sign() {
     console.log(pre.data, sign)
     const req = await api_connection_metamask({ sign: sign })
     console.log("req", req)
+}
+async function metamask_pay_invoices() {
+    await authToken();
+    //Connect the metamask wallet
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    account = accounts[0];
+    console.log("connected :: ", account)
+        //Check the auth token
+    const auth = storage_get_authkey();
+    console.log("auth : ", auth);
+
+    const invoice_id = new URLSearchParams(location.search).get("i")
+
+    console.log("invoices : ", invoice_id)
+    if (invoice_id) {
+        const req = await api_info_invoice(invoice_id)
+        if (req && req.data && req.data.id) {
+            console.log("req", req)
+            invoice = req.data
+        }
+    }
+}
+
+async function metamask_pay_invoice_confirm() {
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    await ethereum
+        .request({
+            method: "eth_sendTransaction",
+            // The following sends an EIP-1559 transaction. Legacy transactions are also supported.
+            params: [{
+                // The user's active address.
+                from: accounts[0],
+                // Required except during contract publications.
+                to: account,
+                // Only required to send ether to the recipient from the initiating external account.
+                value: 0,
+            }, ],
+        })
+        .then((txHash) => console.log(txHash))
+        .catch((error) => console.error(error));
 }
